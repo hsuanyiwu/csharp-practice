f9dda18c82bcc65f83ebfe87f11acd1af95d6b5a
hsuanyiwu <hsuanyiwu@gmail.com>
2022-01-22 19:46:15 +0800

    add process frame
    idea from async/corutine programming with switch/case
    
    demo breakfast and produce/consume problem

---
+++ ProcessStackDemo/FormMain.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ProcessStackDemo
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 12);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(316, 426);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(658, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Breakfast";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnPrepareBreakfast_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnPause_Resume_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(658, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 32);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Produce/Consume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnProcuce_Consume_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(334, 12);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(316, 426);
+            this.txtMsg.TabIndex = 0;
+            this.txtMsg.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtBox);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox txtMsg;
+    }
+}
+
---
+++ ProcessStackDemo/FormMain.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessStackDemo
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            ProcessFrameTime.Elapsed = timer1.Interval;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ProcessFrame.MoveStep();
+            txtBox.Text = ProcessFrame.CallStack();
+        }
+
+        private void btnPrepareBreakfast_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            txtMsg.Clear();
+
+            ProcessFrame.Emit(PrepareBreakfast()).ContinueWith(() =>
+            {
+                button1.Enabled = true;
+            });
+        }
+
+        private ProcessFrame PrepareBreakfast()
+        {
+            var pourCoffee = ProcessFrame.Create("PourCoffee", (p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    Message("coffe prepare");
+                    p.Delay(1000);
+                    break;
+                case 1:
+                    Message("coffe done!");
+                    p.Exit();
+                    break;
+                };
+            });
+
+            var fryEgg = ProcessFrame.Create("FryEgg", (p) =>
+             {
+                 switch (p.Step)
+                 {
+                 case ProcessFrame.ENTER:
+                     Message("fry egg prepare");
+                     p.Delay(5000);
+                     break;
+                 case 1:
+                     Message("fry eggs done!");
+                     p.Exit();
+                     break;
+                 };
+             });
+
+            var breadToast = ProcessFrame.Create("BreadToast", (p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    Message("bread toast prepare");
+                    p.Delay(10000);
+                    break;
+                case 1:
+                    Message("bread toast done!");
+                    p.Exit();
+                    break;
+                };
+            });
+
+            var fryBacon = ProcessFrame.Create("FryBacon", (p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    Message("fry bacon prepare");
+                    p.Delay(8000);
+                    break;
+                case 1:
+                    Message("fry bacon done!");
+                    p.Exit();
+                    break;
+                };
+            });
+
+            return ProcessFrame.Create((p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    Message("breakfast prepare");
+                    ProcessFrame.Emit(breadToast);
+                    p.aWait(pourCoffee);
+                    break;
+                case 1:
+                    p.aWait(fryEgg);
+                    p.aWait(fryBacon);
+                    break;
+                case 2:
+                    p.aWait(breadToast);
+                    break;
+                case 3:
+                    Message("breakfast done!");
+                    p.Exit();
+                    break;
+                }
+            });
+        }
+
+        private void Message(string msg)
+        {
+            var time = DateTime.Now.ToString("mm:ss:ff");
+            txtMsg.Text += $"[{time}] {msg}";
+            txtMsg.Text += Environment.NewLine;
+        }
+
+        private void btnPause_Resume_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            button2.Text = timer1.Enabled ? "Pause" : "Resume";
+        }
+
+        private void btnProcuce_Consume_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            txtMsg.Clear();
+
+            ProcessFrame.Emit(CreateProduceConsume(5)).ContinueWith(() =>
+            {
+                button3.Enabled = true;
+            });
+        }
+
+        private ProcessFrame CreateProduceConsume(int n)
+        {
+            var randon = new Random();
+            var qItems = new Queue<int>();
+            int i = 0;
+
+            var produce = ProcessFrame.Create("Produce", (p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    if (i >= n)
+                    {
+                        qItems.Enqueue(-1);
+                        p.Exit();
+                        return;
+                    }
+                    p.Delay(randon.Next(200, 600));
+                    break;
+                case 1:
+                    qItems.Enqueue(i++);
+                    Message($"producing [ {string.Join(" ", qItems)} ]");
+                    p.SetStep(0);
+                    break;
+                }
+            });
+
+            var consume = ProcessFrame.Create("Consume", (p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    if (qItems.Count > 0)
+                    {
+                        int itemId = qItems.Dequeue();
+                        Message($"consuming [ {string.Join(" ", qItems)} ]");
+                        if (itemId == -1)
+                        {
+                            p.Exit();
+                            return;
+                        }
+                        p.Delay(randon.Next(500, 1000));
+                    }
+                    break;
+
+                case 1:
+                    p.SetStep(0);
+                    break;
+                }
+            });
+
+            var main = ProcessFrame.Create("Main", (p) =>
+            {
+                switch (p.Step)
+                {
+                case ProcessFrame.ENTER:
+                    p.aWait(produce);
+                    p.aWait(consume);
+                    break;
+                case 1:
+                    p.Exit();
+                    break;
+                }
+            });
+            return main;
+        }
+    }
+
+
+}
